6a88f21b75dd50390aac82b36f7a9fa6fe0efd25
incfDevuser <mgomeznavarrocorp@gmail.com>
2024-10-25 01:28:13 -0300

    NavBar - React Router Seteado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import HomePage from "./Pages/HomePage";
+
 function App() {
   return (
     <div>
-      <p className="text-red-700">ola</p>
+      <BrowserRouter>
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      este es el navbar
+    </div>
+  )
+}
+
+export default Navbar
